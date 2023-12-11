32adc4dea65bf1fabc6dd77ffa70defae4a9e714
Fawad-Malik <fawadazher281@outlook.com>
2023-12-11 22:02:00 +0500

    changed structure of project

---
+++ app/blueprints.py
@@ -0,0 +1,35 @@
+from mangum import Mangum
+from fastapi import FastAPI
+from fastapi import Body
+from app.feed_urls import feed_urls
+from app.feed_blc import feed_starter
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+handler = Mangum(app)
+
+
+@app.get("/")
+def home():
+    return {"api_version": "1.0"}
+
+
+@app.get("/sources")
+def sources():
+    news = list(feed_urls.keys())
+    return {"sources": news}
+
+
+@app.get("/ingest")
+def start_feed(
+    data_dict: dict = Body(..., example={"sources": ["genres"]})
+):
+    try:
+        feed_starter(data_dict)
+        return JSONResponse(
+            {"status": "success"},
+        )
+    except Exception as ex:
+        return JSONResponse(
+            {"status": "failed", "Error": str(ex)},
+        )
--- run.py
+++ run.py
@@ -1,48 +1,13 @@
 import os
 import uvicorn
-from mangum import Mangum
-from fastapi import FastAPI, Body
-from feed_urls import feed_urls
-from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
-
 load_dotenv()
-from feed import feed_starter
 
-
-app = FastAPI()
-handler = Mangum(app)
-
-
-@app.get("/")
-def home():
-    return {"api_version": "1.0"}
-
-
-@app.get("/sources")
-def sources():
-    news = list(feed_urls.keys())
-    return {"sources": news}
-
-
-@app.get("/ingest")
-def start_feed(
-    data_dict: dict = Body(..., example={"sources": ["genres"]})
-):
-    try:
-        feed_starter(data_dict)
-        return JSONResponse(
-            {"status": "success"},
-        )
-    except Exception as ex:
-        return JSONResponse(
-            {"status": "failed", "Error": str(ex)},
-        )
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "run:app",
+        "app.blueprints:app",
         host=os.environ.get("HOST"),
         port=int(os.environ.get("PORT")),
         reload=True if os.environ.get("ENVIRONMENT") == "development" else False,
