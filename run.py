21a05cab357ced7a46657268a7f704cf841ac347
Fawad-3gca <107619232+Fawad-3gca@users.noreply.github.com>
2024-08-20 12:22:42 +0500

    added sources with genres api

--- run.py
+++ run.py
@@ -65,6 +65,21 @@ def start_feed(data_dict: dict = Body(..., example={"sources": [], "genres": []}
             {"status": "failed", "Error": str(ex)},
         )
 
+@app.get("/sources-with-genres")
+def get_sources():
+    sources_with_genres = {}
+    for source, details in feed_urls.items():
+        # Initialize the source entry with an empty list
+        sources_with_genres[source] = []
+        genre_set = set()
+        # Extract city names from the set, assuming each set contains only one city name
+        for genres in details["urls"]:
+            # Add the city name to the source's list
+            # breakpoint()
+            if genres["genre"] not in genre_set:
+                sources_with_genres[source].append(genres["genre"])
+                genre_set.add(genres["genre"])
+    return sources_with_genres
 
 if __name__ == "__main__":
     uvicorn.run(
